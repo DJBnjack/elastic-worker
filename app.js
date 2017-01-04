e465a1007d8e7794d6165733d0470b1711dfb55f
DJBnJack <johannes.bertens@gmail.com>
2017-01-04 18:33:15 +0100

    Updated the worker

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ get_and_execute = function() {
     workflows.get_next_task((err, task) => execute_task(err, task, get_and_execute));
 };
 
+var hand_in_results = function(task, callback) {
+    workflows.flag_task_done(task, (error) => {
+        if (error) {
+            console.log("worker:error " + error + " while handing in results.");
+            setTimeout(() => {hand_in_results(task, callback);}, 1000); // retry after 1 sec
+        } else {
+            console.log("worker:done Finished with task " + task.task_id + " for workflow " + task.workflow_id);
+            callback();
+        }
+    });
+};
+
 execute_task = function(err, task, callback) {
     if (err) {
         console.log("worker:error " + err + ", waiting for 15 seconds.");
@@ -24,18 +36,10 @@ execute_task = function(err, task, callback) {
         
         task_executer.execute_task(task, (execute_error) => {
             if (execute_error) {
-                console.log("worker:error " + execute_error + ", waiting for 15 seconds.");
+                console.log("worker:error " + execute_error + " while executing, waiting for 15 seconds.");
                 setTimeout(callback, 15 * 1000);
             } else {
-                workflows.flag_task_done(task, (error) => {
-                    if (error) {
-                        console.log("worker:error " + error + ", waiting for 15 seconds.");
-                        setTimeout(callback, 15 * 1000);
-                    } else {
-                        console.log("worker:done Finished with task " + task.task_id + " for workflow " + task.workflow_id);
-                        callback();
-                    }
-                });
+                hand_in_results(task, callback);
             }
         });
     }
