c5338538413c109d6adfa2f763ca4791f7cff8e6
DJBnJack <johannes.bertens@gmail.com>
2017-03-15 11:15:58 +0100

    More waiting less time

--- app.js
+++ app.js
@@ -5,18 +5,23 @@ task_executer = require('./executer/task_executer');
 console.log("worker:ready Worker started.");
 var Random = require("random-js");
 var random = new Random(Random.engines.mt19937().autoSeed());
-const minWait = 10000; // 10 sec
-const maxWait = 90000; // 90 sec
+const minWait = 5000; // 5 sec
+const maxWait = 60000; // 60 sec
 
 get_and_execute = function() {
-    workflows.get_next_task((err, task) => execute_task(err, task, get_and_execute));
+    var wait = random.integer(minWait, maxWait);
+    console.log("worker:wait Waiting for " + wait + " milliseconds.");
+
+    setTimeout(() => {
+        workflows.get_next_task((err, task) => execute_task(err, task, get_and_execute));
+    }, wait);
 };
 
 var hand_in_results = function(task, callback) {
     workflows.flag_task_done(task, (error) => {
         if (error && error === "hangup") {
-            console.log("worker:error " + error + " while handing in results, retry in 1 second");
-            setTimeout(() => {hand_in_results(task, callback);}, 1000); // retry after 1 sec
+            console.log("worker:error " + error + " while handing in results, retry in 5 second");
+            setTimeout(() => {hand_in_results(task, callback);}, 5000); // retry after 5 sec
         } else if (error) {
             console.log("worker:error " + error + " while handing in results.");
             callback();
@@ -29,25 +34,23 @@ var hand_in_results = function(task, callback) {
 
 
 execute_task = function(err, task, callback) {
-    var wait = random.integer(minWait, maxWait);
-    
     if (err) {
 
-        console.log("worker:error " + err + ", waiting for " + wait + " milliseconds.");
-        setTimeout(callback, wait);
+        console.log("worker:error " + err);
+        callback();
 
     } else if (task === null) {
 
-        console.log("worker:idle No tasks to execute now, waiting for " + wait + " milliseconds.");
-        setTimeout(callback, wait);
+        console.log("worker:idle No tasks to execute now.");
+        callback();
 
     } else {
 
         console.log("worker:start Executing task " + task.task_id + " for workflow " + task.workflow_id);
         task_executer.execute_task(task, (execute_error) => {
             if (execute_error) {
-                console.log("worker:error " + execute_error + " while executing, waiting for " + wait + " milliseconds.");
-                setTimeout(callback, wait);
+                console.log("worker:error " + execute_error + " while executing.");
+                callback();
             } else {
                 hand_in_results(task, callback);
             }
